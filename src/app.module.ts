dece894fca130be1b825887602e3bc480965eea9
Ratu <ratukf@gmail.com>
2026-02-10 17:35:19 +0700

    chore(config): configure TypeOrmModule with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
 
+dotenv.config();
+console.log('DB PASS:', process.env.DB_PASS);
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
