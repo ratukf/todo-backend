36c4a6620faf59a2d8e491d179ffb5b08b56ce5d
Ratu <ratukf@gmail.com>
2026-02-10 23:43:14 +0700

    chore(cors & dto): set cors & dto in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
