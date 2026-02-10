0addd1a5a46cc9feaf97e92d93022f8367a71528
Ratu <ratukf@gmail.com>
2026-02-10 17:59:36 +0700

    feat(todos): create todos entity

---
+++ src/todos/todos.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ default: 'created' })
+  status: string;
+
+  @Column({ default: false })
+  completed: boolean;
+
+  @Column({ default: false })
+  on_going: boolean;
+
+  @Column({ default: '' })
+  problem: string;
+
+  @Column({ nullable: true })
+  problem_desc?: string;
+}
